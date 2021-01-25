0e6743e1a0c3526399c9a857e90f91e05e1d476f
small-lovely-cat <77799160+small-lovely-cat@users.noreply.github.com>
2021-01-25 11:52:07 +0800

    Update report_leave_school.py

--- report_leave_school.py
+++ report_leave_school.py
@@ -128,7 +128,7 @@ def main(cookie):
     Sheng = LastInformation[3]          #省
     Shi = LastInformation[4]            #市
     Xian = LastInformation[5]           #县
-    detailedLocation = "延长中路628弄43号501室"
+    detailedLocation = "延长中路628弄43号501室" #re.findall("\"Text\":\"(.*?)\"", response.text)[1]
     F_State_Shi = json.loads( re.findall("\"F_Items\":(.*?),\"SelectedValueArray\"", response.text)[10])
     F_State_Xian = json.loads(re.findall("\"F_Items\":(.*?),\"SelectedValueArray\"", response.text)[11])
     #print(Sheng, Shi, Xian, detailedLocation, F_State_Shi, F_State_Xian)
