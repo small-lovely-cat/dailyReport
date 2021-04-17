8385149af9664dd3d29c7eb34dafe5fcd5274496
small-lovely-cat <77799160+small-lovely-cat@users.noreply.github.com>
2021-04-17 20:16:54 +0800

    Update report.py

--- report.py
+++ report.py
@@ -5,19 +5,18 @@ import report_leave_school
 import report_at_school
 
 def get_cookies(studentInfo):
-#     header = {
-#         "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.66 Safari/537.36",
-#         }
-#     loginUrl = "https://newsso.shu.edu.cn/login/eyJ0aW1lc3RhbXAiOjE2MDY2NTA3MzEzODc1NDIzODYsInJlc3BvbnNlVHlwZSI6ImNvZGUiLCJjbGllbnRJZCI6IldVSFdmcm50bldZSFpmelE1UXZYVUNWeSIsInNjb3BlIjoiMSIsInJlZGlyZWN0VXJpIjoiaHR0cHM6Ly9zZWxmcmVwb3J0LnNodS5lZHUuY24vTG9naW5TU08uYXNweD9SZXR1cm5Vcmw9JTJmIiwic3RhdGUiOiIifQ=="
-#     data = {"username": studentInfo[0],
-#             "password": studentInfo[1],
-#             }
-#     response = requests.post(loginUrl, headers=header, data=data, allow_redirects=False)
-#     print(response.status_code)
-#     response = requests.get("https://newsso.shu.edu.cn" + response.headers["location"], cookies=response.cookies,allow_redirects=False)
-#     response = requests.get(response.headers["location"], allow_redirects=False)
-      return(studentInfo[2])
-#     return (response.cookies)
+    header = {
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.66 Safari/537.36",
+        }
+    loginUrl = "https://newsso.shu.edu.cn/login/eyJ0aW1lc3RhbXAiOjE2MDY2NTA3MzEzODc1NDIzODYsInJlc3BvbnNlVHlwZSI6ImNvZGUiLCJjbGllbnRJZCI6IldVSFdmcm50bldZSFpmelE1UXZYVUNWeSIsInNjb3BlIjoiMSIsInJlZGlyZWN0VXJpIjoiaHR0cHM6Ly9zZWxmcmVwb3J0LnNodS5lZHUuY24vTG9naW5TU08uYXNweD9SZXR1cm5Vcmw9JTJmIiwic3RhdGUiOiIifQ=="
+    data = {"username": studentInfo[0],
+            "password": studentInfo[1],
+            }
+    response = requests.post(loginUrl, headers=header, data=data, allow_redirects=False)
+    print(response.status_code)
+    response = requests.get("https://newsso.shu.edu.cn" + response.headers["location"], cookies=response.cookies,allow_redirects=False)
+    response = requests.get(response.headers["location"], allow_redirects=False)
+    return (response.cookies)
 
 
 if (__name__ == "__main__"):
@@ -36,8 +35,8 @@ if (__name__ == "__main__"):
     except:
         print("无法获取学号  " + studentInfo[0] + "  的cookie,可能是账号密码错误")
     else:
-#         response = requests.get(url=url,headers=header,cookies=cookie)  #通过跳转地址是每日一报还是两报来判断是否在校
-#         if (response.url=="https://selfreport.shu.edu.cn/DayReport.aspx"):
-#             report_leave_school.main(cookie,detailedLocation)
-#         elif (response.url==url):
+        response = requests.get(url=url,headers=header,cookies=cookie)  #通过跳转地址是每日一报还是两报来判断是否在校
+        if (response.url=="https://selfreport.shu.edu.cn/DayReport.aspx"):
+            report_leave_school.main(cookie,detailedLocation)
+        elif (response.url==url):
             report_at_school.main(cookie)
