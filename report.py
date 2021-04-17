dd7d98746f6062999ab4b03fea635fec21a61508
small-lovely-cat <77799160+small-lovely-cat@users.noreply.github.com>
2021-04-17 17:31:07 +0800

    Update report.py

--- report.py
+++ report.py
@@ -18,7 +18,7 @@ def get_cookies(studentInfo):
     #response = requests.get("https://newsso.shu.edu.cn" + response.headers["location"], cookies=response.cookies,
                             #allow_redirects=False)
     #response = requests.get(response.headers["location"], allow_redirects=False)
-    response = requests.get("oauth/authorize?client_id=WUHWfrntnWYHZfzQ5QvXUCVy&response_type=code&scope=1&redirect_uri=https%3A%2F%2Fselfreport.shu.edu.cn%2FLoginSSO.aspx%3FReturnUrl%3D%252f&state=", allow_redirects=False)
+    #response = requests.get("oauth/authorize?client_id=WUHWfrntnWYHZfzQ5QvXUCVy&response_type=code&scope=1&redirect_uri=https%3A%2F%2Fselfreport.shu.edu.cn%2FLoginSSO.aspx%3FReturnUrl%3D%252f&state=", allow_redirects=False)
     return (response.cookies)
 
 
